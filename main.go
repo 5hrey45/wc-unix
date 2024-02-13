3382e3a1fe534f14c9f88f13374cb27bc1e772b5
Shreyas S S <shreyasnarve1611@gmail.com>
2024-02-13 22:02:18 +0530

    initial commit: coded wc -c filename funcionality

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+
+	arglen := len(os.Args)
+	if arglen < 1 {
+		log.Fatal("No arguments specified")
+	}
+
+	file := os.Args[1]
+
+	f, err := os.Stat(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(f.Size(), file)
+}
