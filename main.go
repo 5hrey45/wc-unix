3528ad0985636b07cb3c1c2012fc97ef3fa51255
Shreyas S S <shreyasnarve1611@gmail.com>
2024-02-13 22:35:28 +0530

    refactored code by adding functions

--- main.go
+++ main.go
@@ -9,16 +9,41 @@ import (
 func main() {
 
 	arglen := len(os.Args)
-	if arglen < 2 {
+	args := os.Args
+	var flag string
+	var filename string
+
+	if arglen == 1 {
 		log.Fatal("No arguments specified")
-	}
+	} else if arglen == 2 {
+		filename = args[1]
+		// Todo: needs to implement wc filename
+		// output would be lines, words, chars, filename
+	} else if arglen == 3 {
+		flag = args[1]
+		filename = args[2]
+	
+		switch flag {
+		case "-c":
+			getBytes(filename)
+		case "-w":
+			// getWords(filename)
+		case "-l":
+			// getLines(filename)
+		case "-m":
+			getBytes(filename)
+		default:
+			log.Fatal("Flag provided but not defined")
+		}		
+	}	
+}
 
-	file := os.Args[1]
-
-	f, err := os.Stat(file)
+func getBytes(filename string) {
+	f, err := os.Stat(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(f.Size(), file)
+	fmt.Println(f.Size(), filename)
 }
+
